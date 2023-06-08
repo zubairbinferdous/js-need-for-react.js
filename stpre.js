884f7c476d4aa77cac671eac811daf249eb33553
zubairbinferdous <zubidev71@gmail.com>
2023-06-08 06:36:33 +0600

    ok

---
+++ stpre.js
@@ -0,0 +1,17 @@
+const addToLocalStorage = () => {
+    const idInput = document.getElementById('storage-id');
+    const id = idInput.value;
+    const valueInput = document.getElementById('storage-value');
+    const value = valueInput.value;
+
+    //
+    if (id && value) {
+        localStorage.setItem(id, value);
+    }
+    idInput.value = '';
+    valueInput.value = '';
+}
+
+Array , object 
+JSON.stringify()
+JSON.parse()
